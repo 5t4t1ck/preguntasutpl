450c062d80101e68fc550ace0fdd334b04af5833
5t4t1ck <dsaavedra88@gmail.com>
2016-10-17 12:40:15 -0500

    Cambios por parte de Jimmy

--- categorias.php
+++ categorias.php
@@ -1,12 +1,12 @@
-<?php
-
+<?php	
+		
 		$cat1=1;
 		$cat2=1;
 		$cat3=1;
 		$cat4=1;
 			if ($cat1==1){
 				echo '	<div class="col-xs-6">
-							<a href="radio.php" class="btn btn-lg btn-block btn-outline">
+							<a href="radio" class="btn btn-lg btn-block btn-outline">
 								<img src="img/radio.svg" alt="" style="display:inline; padding-right: 2%">Radio
 							</a>
 						</div>';
@@ -17,10 +17,10 @@
 							</a>
 						</div>';
 			}
-
+			
 			if ($cat2==1){
 				echo '	<div class="col-xs-6">
-							<a href="historia.php" class="btn btn-lg btn-block btn-outline">
+							<a href="historia" class="btn btn-lg btn-block btn-outline">
 								<img src="img/historia.svg" alt="" style="display:inline; padding-right: 2%">Historia
 							</a>
 						</div>';
@@ -33,7 +33,7 @@
 			}
 			if ($cat3==1){
 				echo '	<div class="col-xs-6">
-							<a href="tv.php" class="btn btn-lg btn-block btn-outline">
+							<a href="tv" class="btn btn-lg btn-block btn-outline">
 								<img src="img/tv.svg" alt="" style="display:inline; padding-right: 2%">TV
 							</a>
 						</div>';
@@ -46,7 +46,7 @@
 			}
 			if ($cat4==1){
 				echo '	<div class="col-xs-6">
-							<a href="teorias.php" class="btn btn-lg btn-block btn-outline">
+							<a href="teorias" class="btn btn-lg btn-block btn-outline">
 								<img src="img/teorias.svg" alt="" style="display:inline; padding-right: 2%">Teorías
 							</a>
 						</div>';
@@ -56,5 +56,5 @@
 								<img src="img/teorias-lock.svg" alt="" style="display:inline; padding-right: 2%">Teorías
 							</a>
 						</div>';
-			}
+			}	
 ?>
